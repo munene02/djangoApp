e36929d2d0bbbeb9cb38e82e3fc025f4b4334f11
Martin Munene <munene02@gmail.com>
2024-10-07 13:07:59 +0300

    add post page

--- posts/views.py
+++ posts/views.py
@@ -6,6 +6,7 @@ def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
-def post_detail(request, id):
-    return render(request, 'posts/posts_detail.html', {'id': id})
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
 
